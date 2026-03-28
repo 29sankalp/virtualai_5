586fc9a5720717ab264fc69f2402da36321a92c4
29sankalp <sankalpjoshi622@gmail.com>
2026-03-29 02:17:24 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -53,8 +53,8 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "Strict",
-      secure: false
+      sameSite: "None",
+      secure: true
     });
 
     return res.status(200).json(user);
@@ -81,7 +81,8 @@ try {
 
   const token = await genToken(user._id);
   res.cookie("token",token,{
-    httpOnly:true,maxAge:7*24*60*60*1000,sameSite:"Strict",secure:false
+    httpOnly:true,maxAge:7*24*60*60*1000,      sameSite: "None",
+      secure: true
   })
   return res.status(200).json(user)
 } catch (error) {
